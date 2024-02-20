e3cb62b13c5a3186cb8069bbd0c02b348a715159
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-02-20 16:27:46 +0000

    Create gene page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePageView from "../views/HomePageView.vue";
 import PublicationsView from "../views/PublicationsView.vue";
+import GeneView from "../views/GeneView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/publications",
       name: "publications",
       component: PublicationsView,
+    },
+    {
+      path: "/gene/:symbol",
+      name: "gene symbol",
+      component: GeneView,
     },
   ],
 });
