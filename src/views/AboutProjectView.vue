cc9ab4e7c43ac316c5ac74c869476e096dfb47bc
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2024-03-01 17:42:10 +0000

    Adding documentation to the project page

--- src/views/AboutProjectView.vue
+++ src/views/AboutProjectView.vue
@@ -3,4 +3,32 @@ import About from './AboutView.vue';
 </script>
 <template>
     <About />
+    <div class="container" style="vertical-align: middle">
+        <body>
+            <p>
+                The G2P project shares detailed, evidence-based gene-disease models, curated from the literature by experts. 
+                These data can be used in the G2P extension of the Ensembl Variant Effect Predictor (Ensembl VEP) to efficiently filter genotype data from the exome or genome wide sequencing of people with genetic disorders to identify likely causative variants and accelerate diagnosis.
+            </p>
+            <p>
+            The project was established in 2014, focused on developmental disorders and has since been generalised to cover other disease areas. All data is made freely available. The initial paper is available <a href="https://doi.org/10.1038/s41467-019-10016-3">here.</a>
+            </p>
+            <p>
+                We report:
+                <ul>
+                    <li>Allelic requirement to enable more precise matching on genotype</li>
+                    <li>Variant molecular consequence to enable efficient variant filtering</li>
+                    <li>Mutation mechanism, with evidence</li>
+                    <li>Confidence, based on the strength of evidence available in the literature</li>
+                    <li>Additional information to aid clinical interpretation including when a genotype is only seen de novo and observations of reduced penetrance, mosaicism</li>
+                    <li>The publications used in assessment</li>
+                </ul>
+                The full curation process is described here and terminology used is described <RouterLink to="/about/terminology">here</RouterLink>
+            </p>
+            <p>
+                The Ensembl VEP diagnostic variant filtering pipeline matches patient genotypes to allelic requirements and genes reported in the literature to identify likely causative genes and variants. Full information is available 
+                <RouterLink to="/about/variant-filtering">here</RouterLink>
+            </p>
+        </body>
+    </div>
+
 </template>
