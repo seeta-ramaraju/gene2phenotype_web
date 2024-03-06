2f26b41048549f561a5114501d724669564b3382
Ola Austine <83219714+olaaustine@users.noreply.github.com>
2024-03-06 15:13:59 +0000

    Removing stuff from the router

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePageView from "../views/HomePageView.vue";
 import PublicationsView from "../views/PublicationsView.vue";
 import AboutProjectView from "../views/AboutProjectView.vue";
-import AboutTerminologyView from "../views/AboutTerminologyView.vue";
-import AboutVariantFilteringView from "../views/AboutVariantFilteringView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,16 +20,6 @@ const router = createRouter({
       path: "/about/project",
       name: "aboutproject",
       component: AboutProjectView,
-    },
-    {
-      path: "/about/terminology",
-      name: "aboutterminology",
-      component: AboutTerminologyView,
-    },
-    {
-      path: "/about/variant-filtering",
-      name: "aboutvariantfiltering",
-      component: AboutVariantFilteringView,
     },
   ],
 });
