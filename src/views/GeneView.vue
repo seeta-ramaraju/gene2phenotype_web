1f70d695f2016d14a1c4027bc67687a8653b4fbd
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-03-08 16:34:48 +0000

    Update gene page

--- src/views/GeneView.vue
+++ src/views/GeneView.vue
@@ -6,6 +6,12 @@ export default {
       geneSummaryData: null,
       geneData: null,
       errorMsg: null,
+      confidenceColorMap: {
+        definitive: "green",
+        strong: "green",
+        moderate: "green",
+        limited: "red",
+      },
     };
   },
   created() {
@@ -59,14 +65,14 @@ export default {
     <div v-if="isDataLoading">Data is loading...Please wait...</div>
     <div v-if="errorMsg" class="text-danger">{{ errorMsg }}</div>
     <div v-if="geneData && geneSummaryData">
-      <h3 v-if="geneData.gene_symbol">{{ geneData.gene_symbol }}</h3>
-      <h3 v-else>Not Available</h3>
+      <h2 v-if="geneData.gene_symbol">{{ geneData.gene_symbol }}</h2>
+      <h2 v-else class="text-muted">Not Available</h2>
       <h4 class="py-3">Other Synonyms</h4>
       <div class="row">
         <p v-if="geneData.synonyms && geneData.synonyms.length > 0">
           {{ geneData.synonyms.join(", ") }}
         </p>
-        <p v-else style="color: grey">Not Available</p>
+        <p v-else class="text-muted">Not Available</p>
       </div>
       <h4 class="py-3">Latest Records</h4>
       <div class="row mx-1">
@@ -84,7 +90,10 @@ export default {
               <th>Variant Consequence</th>
               <th>Variant Type</th>
               <th>Mechanism</th>
-              <th>Confidence</th>
+              <th>
+                Confidence
+                <a href="#"><i class="bi bi-question-circle-fill"></i></a>
+              </th>
               <th>Panels</th>
               <th>Record Page</th>
             </tr>
@@ -95,29 +104,45 @@ export default {
               <td>{{ item.genotype }}</td>
               <td>{{ item.variant_consequence.join(", ") }}</td>
               <td>{{ item.variant_type.join(", ") }}</td>
-              <td></td>
-              <td>{{ item.confidence }}</td>
+              <td>{{ item.molecular_mechanism.join(", ") }}</td>
+              <td>
+                <span
+                  :style="{
+                    color: confidenceColorMap[item.confidence.toLowerCase()],
+                  }"
+                  v-if="item.confidence"
+                >
+                  {{ item.confidence }}
+                </span>
+              </td>
               <td>{{ item.panels.join(", ") }}</td>
               <td>
-                <button class="btn btn-primary">
+                <router-link
+                  :to="`/lgd/${item.stable_id}`"
+                  class="btn btn-primary"
+                  v-if="item.stable_id"
+                >
                   View <i class="bi bi-box-arrow-up-right"></i>
-                </button>
+                </router-link>
               </td>
             </tr>
           </tbody>
         </table>
-        <p v-else style="color: grey">Not Available</p>
+        <p v-else class="text-muted">Not Available</p>
       </div>
       <h4 class="py-3">Additional Links</h4>
       <div class="row mx-3 pb-3">
         <ul>
-          <li>
-            <a href="#" style="text-decoration: none">
+          <li v-if="geneData.gene_symbol">
+            <a
+              v-bind:href="`https://www.deciphergenomics.org/gene/${geneData.gene_symbol}`"
+              style="text-decoration: none"
+            >
               View this gene or submit patient variants via DECIPHER
               <i class="bi bi-box-arrow-up-right"></i>
             </a>
           </li>
-          <li v-if="geneData.ids.Ensembl">
+          <li v-if="geneData.ids?.Ensembl">
             <a
               v-bind:href="`https://www.ensembl.org/Homo_sapiens/Gene?g=${geneData.ids.Ensembl}`"
               style="text-decoration: none"
@@ -126,7 +151,7 @@ export default {
               <i class="bi bi-box-arrow-up-right"></i>
             </a>
           </li>
-          <li v-if="geneData.ids.HGNC">
+          <li v-if="geneData.ids?.HGNC">
             <a
               v-bind:href="`https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/HGNC:${geneData.ids.HGNC}`"
               style="text-decoration: none"
@@ -142,7 +167,7 @@ export default {
         <span v-if="geneData.last_updated">
           {{ geneData.last_updated }}
         </span>
-        <span v-else style="color: grey">Not Available</span>
+        <span v-else class="text-muted">Not Available</span>
       </p>
     </div>
   </div>
