2724cc040471eb4cf344d617eaf05bd9607bda5c
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-04-09 10:00:33 +0100

    Update About Project page

--- src/views/AboutProjectView.vue
+++ src/views/AboutProjectView.vue
@@ -1,40 +1,59 @@
 <template>
-    <div class="container px-5 py-3" style="vertical-align: middle">
-        <h5>The Project</h5>
-        <p>
-            The G2P project shares detailed, evidence-based gene-disease models, curated from the literature by experts.
-            These data can be used in the G2P extension of the Ensembl Variant Effect Predictor (Ensembl VEP) to
-            efficiently filter genotype data from the exome or genome wide sequencing of people with genetic disorders
-            to identify likely causative variants and accelerate diagnosis.
-        </p>
-        <p>
-            The project was established in 2014, focused on developmental disorders and has since been generalised to
-            cover other disease areas. All data is made freely available. The initial paper is available <a
-                href="https://doi.org/10.1038/s41467-019-10016-3">here.</a>
-        </p>
-        <p>
-            We report:
-        <ul>
-            <li>Allelic requirement to enable more precise matching on genotype</li>
-            <li>Variant molecular consequence to enable efficient variant filtering</li>
-            <li>Mutation mechanism, with evidence</li>
-            <li>Confidence, based on the strength of evidence available in the literature</li>
-            <li>Additional information to aid clinical interpretation including when a genotype is only seen de novo and
-                observations of reduced penetrance, mosaicism</li>
-            <li>The publications used in assessment</li>
-        </ul>
-        The full curation process is described here and terminology used is described <RouterLink to="#">here
-        </RouterLink>
-        </p>
-        <p>
-            The Ensembl VEP diagnostic variant filtering pipeline matches patient genotypes to allelic requirements and
-            genes reported in the literature to identify likely causative genes and variants. Full information is
-            available
-            <RouterLink to="#">here</RouterLink>
-        </p>
-        <a href="https://thegencc.org/">
-            <img alt="GenCC logo" src="../assets/GenCC.png" width="20%" height="auto" /> </a> G2P is a member of the
-        Gene Curation Coalition which seeks to standardise the curation and description of gene disease information
-
-    </div>
+  <div class="container px-5 py-3">
+    <h2>The Project</h2>
+    <p>
+      The G2P project shares detailed, evidence-based gene-disease models,
+      curated from the literature by experts. These data can be used in the G2P
+      extension of the Ensembl Variant Effect Predictor (Ensembl VEP) to
+      efficiently filter genotype data from the exome or genome wide sequencing
+      of people with genetic disorders to identify likely causative variants and
+      accelerate diagnosis.
+    </p>
+    <p>
+      The project was established in 2014, focused on developmental disorders
+      and has since been generalised to cover other disease areas. All data is
+      made freely available. The initial paper is available
+      <a href="https://doi.org/10.1038/s41467-019-10016-3">here.</a>
+    </p>
+    <p>We report:</p>
+    <ul>
+      <li>Allelic requirement to enable more precise matching on genotype</li>
+      <li>
+        Variant molecular consequence to enable efficient variant filtering
+      </li>
+      <li>Mutation mechanism, with evidence</li>
+      <li>
+        Confidence, based on the strength of evidence available in the
+        literature
+      </li>
+      <li>
+        Additional information to aid clinical interpretation including when a
+        genotype is only seen de novo and observations of reduced penetrance,
+        mosaicism
+      </li>
+      <li>The publications used in assessment</li>
+    </ul>
+    <p>
+      The full curation process is described here and terminology used is
+      described <a href="#">here</a>
+    </p>
+    <p>
+      The Ensembl VEP diagnostic variant filtering pipeline matches patient
+      genotypes to allelic requirements and genes reported in the literature to
+      identify likely causative genes and variants. Full information is
+      available <a href="#">here</a>
+    </p>
+    <a href="https://thegencc.org/">
+      <img
+        alt="GenCC logo"
+        src="../assets/GenCC.png"
+        width="20%"
+        height="auto"
+      />
+    </a>
+    <span>
+      G2P is a member of the Gene Curation Coalition which seeks to standardise
+      the curation and description of gene disease information
+    </span>
+  </div>
 </template>
