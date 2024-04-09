c2185754d2baab03d149b61f2b17c1528b46cc6e
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-04-09 10:23:26 +0100

    Update HeaderWithoutSearch component

--- src/components/header/HeaderWithoutSearch.vue
+++ src/components/header/HeaderWithoutSearch.vue
@@ -2,21 +2,41 @@
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand">
-        <img src="../../assets/G2P-logo.png" alt="G2P logo" width="30" height="30"
-          class="d-inline-block align-text-top" />
+        <img
+          src="../../assets/G2P-logo.png"
+          alt="G2P logo"
+          width="30"
+          height="30"
+          class="d-inline-block align-text-top"
+        />
         Gene2Phenotype
       </router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mb-2 mb-lg-0 me-auto">
           <li class="nav-item">
-            <router-link to="/" class="nav-link active" aria-current="page">Home</router-link>
+            <router-link to="/" class="nav-link active" aria-current="page"
+              >Home</router-link
+            >
           </li>
           <li class="nav-item dropdown">
-            <span class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <span
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
               About
             </span>
             <ul class="dropdown-menu">
@@ -28,22 +48,11 @@
               <li><a class="dropdown-item" href="#">Terminology</a></li>
               <li><a class="dropdown-item" href="#">Variant filtering</a></li>
               <li>
-                <router-link to="/publications" class="dropdown-item">Publications</router-link>
+                <router-link to="/publications" class="dropdown-item"
+                  >Publications</router-link
+                >
               </li>
               <li><a class="dropdown-item" href="#">Announcements</a></li>
-            </ul>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Browse
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">All panels</a></li>
-              <li>
-                <hr class="dropdown-divider" />
-              </li>
-              <li><a class="dropdown-item" href="#">Cancer panel</a></li>
-              <li><a class="dropdown-item" href="#">Eye panel</a></li>
             </ul>
           </li>
         </ul>
