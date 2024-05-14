5495a925b1766bdae905e082a0f37920d60b7684
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-05-14 15:45:29 +0100

    Rename header text from Gene Function to Function in Gene page

--- src/views/GeneView.vue
+++ src/views/GeneView.vue
@@ -93,7 +93,7 @@ export default {
         </p>
         <p v-else class="text-muted">Not Available</p>
       </div>
-      <h4 class="py-3">Gene Function</h4>
+      <h4 class="py-3">Function</h4>
       <div class="row">
         <p v-if="geneFunctionData?.function?.protein_function">
           {{ geneFunctionData.function.protein_function }}
