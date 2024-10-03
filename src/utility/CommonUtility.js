87c19ce7d6ce8956aa1b6c98cd4b89fa0f95bb61
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-10-03 20:15:59 +0100

    delete file

--- src/utility/CommonUtility.js
+++
@@ -1,7 +0,0 @@
-export const getEvidenceTypes = (evidenceObj) => {
-  let evidenceTypes = [];
-  Object.values(evidenceObj).forEach((value) => {
-    evidenceTypes = evidenceTypes.concat(value);
-  });
-  return evidenceTypes;
-};
