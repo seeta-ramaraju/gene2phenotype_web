20ec4b7bcc9af890f2ed9e893a1f1c4c540bf78e
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-12-24 16:12:16 +0000

    create 404 error page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="container px-5 py-3 d-flex align-items-center justify-content-center"
+    style="min-height: 60vh"
+  >
+    <div class="text-center">
+      <h1 class="fw-bold display-1">404</h1>
+      <h2 class="text-secondary fs-2">Page Not Found</h2>
+      <p class="mb-4 lead">
+        The page you're looking for doesn't exist or has been moved.
+      </p>
+      <router-link to="/" class="btn btn-primary"> Return Home </router-link>
+    </div>
+  </div>
+</template>
