12379cc29097fec88a1103edc5cd59f8e9332b5e
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2025-01-06 17:45:05 +0000

    Change HELP_TEXT constant

--- src/utility/Constants.js
+++ src/utility/Constants.js
@@ -11,14 +11,14 @@ export const MAX_CHARACTERS = 800;
 
 export const HELP_TEXT = {
   ALLELIC_REQUIREMENT:
-    "Required genotype. G2P uses synonyms of the HPO mode of inheritance terminology is used as many of the disorders described are de novo.",
+    "Required genotype. G2P uses synonyms of the HPO mode of inheritance terminology, as many of the disorders described are de novo.",
   VARIANT_CONSEQUENCE:
-    "The consequence of the reported variants at the protein (for protein-coding genes) or the RNA (for non-protein coding genes), per allele.",
+    "The consequence of the reported variants on the protein (for protein-coding genes) or the RNA (for non-protein coding genes), per allele.",
   VARIANT_TYPE:
     "The types of variants associated with the curated gene-disease pair reported in the publication",
   MECHANISM:
     "The molecular mechanisms underlying gene-disease association  Derived from Badonyi & Marsh.",
-  CONFIDENCE: "Confidence the assosiation is real, based on available evidence",
+  CONFIDENCE: "Confidence the association is real, based on available evidence",
   CROSS_CUTTING_MODIFIER:
     "Additional information reported in the publication that is relevant to the gene-disease inheritance",
   CATEGORIZATION:
